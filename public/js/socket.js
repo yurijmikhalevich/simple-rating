6a5db052b31a4e9763c60f6bce4141595aac6371
Yurij Mikhalevich <0@39.yt>
2013-12-04 23:04:01 +0400

    Domain changed from test to work.

--- public/js/socket.js
+++ public/js/socket.js
@@ -3,7 +3,7 @@
  * @author 0@39.yt (Yurij Mikhalevich)
  */
 
-var socket = io.connect('http://simple-rating/');
+var socket = io.connect('http://kittens-are-on-the-way.org/');
 
 socket.on('err', function(error) {
   console.err(error);
